1b8261855b6fc8c28c5f7d2056b34c3b85499368
lazytrainer09 <wihan0927@gmail.com>
2022-05-05 22:59:57 +0900

    section2.6.2

---
+++ 02-sec/javascripts/shallow-copy.js
@@ -0,0 +1,14 @@
+const patty = {
+  name: 'Patty Rabbit',
+  email: 'patty@maple.town',
+  address: {
+    town: 'Maple Town'
+  },
+};
+
+const rolley = { ...patty, name: 'Rolley Cocker' };
+rolley.email = 'rolley@palm.town';
+rolley.address.town = 'Palm Town';
+
+console.log(patty);
+console.log(rolley);
