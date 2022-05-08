3f43f272998a4a85b54b08293759d2bd27b060d5
lazytrainer09 <wihan0927@gmail.com>
2022-05-08 13:30:26 +0900

    2.9.3

---
+++ 02-sec/esm/calc2.js
@@ -0,0 +1,5 @@
+import { add, multiply, TWO, TEN, German } from './modules/index.js';
+
+console.log(add(91, TWO)); // 93
+console.log(multiply(5, TEN)); // 50
+console.log(multiply(German.FUNF, German.DREI)); // 15
